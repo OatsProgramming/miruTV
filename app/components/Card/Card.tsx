3a79f489cb9b45a20d0b795300f024712068113b
Jack Oats <jackoatmeals@gmail.com>
2023-06-03 03:09:02 -0500

    update: can pass sources as params

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -1,18 +1,31 @@
+import Link from 'next/link'
 import styles from './card.module.css'
 
-export default function Card({ img, title }: {
-    img: string,
-    title: string,
+export default function Card({ coverImg, animeTitle, epTitle, epNumber, sources, animeId }: {
+    coverImg: string,
+    animeTitle: string,
+    epTitle?: string,
+    epNumber?: number,
+    // recentList
+    sources?: AnimeSourcePlain[],
+    // popularList
+    animeId?: string,
 }) {
+    const sourcesJSON = encodeURIComponent(JSON.stringify(sources))
+    const link = sources ? `/watch/${sourcesJSON}` : `/`
+
     return (
-        <div className={styles['card']}>
-            <img
-                loading='lazy'
-                src={img}
-                width={300}
-                height={400}
-            />
-            <h2>{title}</h2>
-        </div>
+        <Link href={link}>
+            <div className={styles['card']}>
+                <img
+                    loading='lazy'
+                    src={coverImg}
+                />
+                {epNumber && (
+                    <h3>EP {epNumber}: {epTitle ?? 'N/A'}</h3>
+                )}
+                <h2>{animeTitle}</h2>
+            </div>
+        </Link>
     )
 }
