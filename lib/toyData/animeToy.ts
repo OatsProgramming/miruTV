c646d7877e2a22a5b0bcde925c133c38762890fa
Jack Oats <jackoatmeals@gmail.com>
2023-06-05 04:45:11 -0500

    update: change in data structure due to api. changing toyData

--- lib/toyData/animeToy.ts
+++ lib/toyData/animeToy.ts
@@ -1,247 +1,269 @@
-const animeToy = [
-    {
+const animeToy = {
+    "id": "clfrutw900067jslualgc42tu",
+    "slug": "i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too",
+    "anilistId": 153845,
+    "coverImage": "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx153845-C47aoKy7wf19.jpg",
+    "bannerImage": "https://s4.anilist.co/file/anilistcdn/media/anime/banner/153845-qcUXgT0toF1J.jpg",
+    "status": "RELEASING",
+    "season": "SPRING",
+    "title": {
+      "native": "異世界でチート能力を手にした俺は、現実世界をも無双する",
+      "romaji": "Isekai de Cheat Skill wo Te ni Shita Ore wa, Genjitsu Sekai wo mo Musou Suru: Level Up wa Jinsei wo Kaeta",
+      "english": "I Got a Cheat Skill in Another World and Became Unrivaled in The Real World, Too",
+      "userPreferred": "Isekai de Cheat Skill wo Te ni Shita Ore wa, Genjitsu Sekai wo mo Musou Suru: Level Up wa Jinsei wo Kaeta"
+    },
+    "mappings": {
+      "mal": 52830,
+      "anidb": 17580,
+      "kitsu": 46370,
+      "anilist": 153845,
+      "thetvdb": 423653,
+      "anisearch": 17627,
+      "livechart": 11384,
+      "notify.moe": "0bZQAoKVg",
+      "anime-planet": "i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too"
+    },
+    "currentEpisode": 9,
+    "next": "2023-06-05T15:00:00.000Z",
+    "synonyms": [
+      "I Got a Cheat Ability in a Different World, and Became Extraordinary Even in the Real World",
+      "Isekai de Cheat Nouryoku Ote ni Shita Ore wa, Genjitsu Sekai o mo Musou Suru",
+      "สกิลโกงไร้เทียมทาน สร้างตำนานในสองโลก: ชีวิตพลิกผันด้วยการอัปเลเวล",
+      "Ganhei um Poder Apelão em Outro Mundo e Agora Sou Imbatível no Mundo Real",
+      "Iseleve",
+      "在异世界获得超强能力的我，在现实世界照样无敌～等级提升改变人生命运"
+    ],
+    "countryOfOrigin": "JP",
+    "lastEpisodeUpdate": "2023-05-29T17:43:21.040Z",
+    "seasonInt": 232,
+    "description": "All his life, Yuuya has been bullied at school and neglected by his parents. After moving into his late grandfather’s home, he discovers a strange door that seems to be calling out to him. Yuuya opens the door, steps inside, and is suddenly transported to a magical world! This new world holds rare treasures and grants him powerful skills, which he takes back to Earth for a new lease on life.<br>\n<br>\n(Source: Crunchyroll)",
+    "duration": 24,
+    "averageScore": 66,
+    "popularity": 52370,
+    "color": "#86281a",
+    "year": 2023,
+    "createdAt": "2023-03-28T06:05:51.111Z",
+    "updatedAt": "2023-06-04T20:50:40.501Z",
+    "format": "TV",
+    "lastChecks": {
+      "clfrxh9h51slsluksocdfxs09": 1685382372977,
+      "clfrxh9le1sluluks7p0eduer": 1685382375599
+    },
+    "genre": [
+      "Action",
+      "Fantasy",
+      "Adventure",
+      "Romance"
+    ],
+    "episodes": [
+      {
         "id": "clg100bxdsevgpk01b0xsmqur",
         "number": 1,
         "title": "To Another World",
         "titleVariations": {
-            "native": "異世界へ",
-            "english": "To Another World"
+          "native": "異世界へ",
+          "english": "To Another World"
         },
         "description": "Tenjou Yuuya has been bullied all his life. Then he discovers a door to another world. Powerful weapons. Vicious monsters. It all seems like a fantasy game! And when he returns to his world, he realizes that his leveling up comes with him.",
         "image": "https://artworks.thetvdb.com/banners/v4/episode/9479308/screencap/64297ee741c21.jpg",
-        "airedAt": "2023-04-03T00:00:00.000Z",
         "createdAt": "2023-04-03T15:40:45.025Z",
+        "airedAt": "2023-04-03T00:00:00.000Z",
         "sources": [
-            {
-                "id": "clg1259im0000mbpk8kug30ph",
-                "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-1",
-                "priority": 1,
-                "url": "https://gogoanime.cl"
-            },
-            {
-                "id": "clgnvua6600094cpk3stscko0",
-                "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=99965",
-                "priority": 2,
-                "url": "https://zoro.to"
-            }
+          {
+            "id": "clg1259im0000mbpk8kug30ph",
+            "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-1"
+          },
+          {
+            "id": "clgnvua6600094cpk3stscko0",
+            "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=99965"
+          }
         ]
-    },
-    {
+      },
+      {
         "id": "clgaynrkbs263pk016kdewfk9",
         "number": 2,
         "title": "Ousei Academy",
         "titleVariations": {
-            "english": "Ousei Academy",
-            "japanese": "王星学園"
+          "english": "Ousei Academy",
+          "japanese": "王星学園"
         },
         "description": "When Yuuya reunites with the girl he saved back at the convenience store, she invites him to attend the prestigious Ousei Academy. Yuuya isn’t sure that he’s worthy of attending a school full of geniuses, but he’s persuaded to try it for just one day.",
         "image": "https://artworks.thetvdb.com/banners/v4/episode/9698057/screencap/6430574a709fa.jpg",
-        "airedAt": "2023-04-11T00:00:00.000Z",
         "createdAt": "2023-04-10T15:00:40.906Z",
+        "airedAt": "2023-04-11T00:00:00.000Z",
         "sources": [
-            {
-                "id": "clgb3oyva0001gkpk662j2jwd",
-                "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-2",
-                "priority": 1,
-                "url": "https://gogoanime.cl"
-            },
-            {
-                "id": "clgnvua6l000a4cpkebnt8run",
-                "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=100021",
-                "priority": 2,
-                "url": "https://zoro.to"
-            }
+          {
+            "id": "clgb3oyva0001gkpk662j2jwd",
+            "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-2"
+          },
+          {
+            "id": "clgnvua6l000a4cpkebnt8run",
+            "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=100021"
+          }
         ]
-    },
-    {
+      },
+      {
         "id": "clgkyqpyw6n5ipk010rhak14o",
         "number": 3,
         "title": "Life Changes",
         "titleVariations": {
-            "native": "人生の変化",
-            "english": "Life Changes"
+          "native": "人生の変化",
+          "english": "Life Changes"
         },
         "description": "Yuuya realizes that he doesn’t have many clothes that fit his new, leveled-up height. So, he heads to the mall where, in an unexpected turn of events, he ends up participating in a fashion photoshoot opposite a super popular female model?!",
         "image": "https://artworks.thetvdb.com/banners/v4/episode/9698058/screencap/64395095d9ff5.jpg",
-        "airedAt": "2023-04-21T00:00:00.000Z",
         "createdAt": "2023-04-17T15:00:40.568Z",
+        "airedAt": "2023-04-21T00:00:00.000Z",
         "sources": [
-            {
-                "id": "clgl3sq2v0000otpk8ag49ze5",
-                "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-3",
-                "priority": 1,
-                "url": "https://gogoanime.cl"
-            },
-            {
-                "id": "clgnvua6v000b4cpk2rfs9c15",
-                "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=100215",
-                "priority": 2,
-                "url": "https://zoro.to"
-            }
+          {
+            "id": "clgl3sq2v0000otpk8ag49ze5",
+            "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-3"
+          },
+          {
+            "id": "clgnvua6v000b4cpk2rfs9c15",
+            "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=100215"
+          }
         ]
-    },
-    {
+      },
+      {
         "id": "clguytoiugxzapk01tgbp79jj",
         "number": 4,
         "title": "A Step of Courage",
         "titleVariations": {
-            "native": "勇気の一歩",
-            "english": "A Step of Courage"
+          "native": "勇気の一歩",
+          "english": "A Step of Courage"
         },
         "description": "Yuya responds to Princess Lexia's marriage proposal and receives an invitation to an audience at the royal palace. At the same time, he returned to the Ôsei Academy.",
         "image": "https://artworks.thetvdb.com/banners/v4/episode/9698059/screencap/6463a8803a702.jpg",
-        "airedAt": "2023-04-28T00:00:00.000Z",
         "createdAt": "2023-04-24T15:00:40.422Z",
+        "airedAt": "2023-04-28T00:00:00.000Z",
         "sources": [
-            {
-                "id": "clgv3xxxx000169pkgb5rdn5l",
-                "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-4",
-                "priority": 1,
-                "url": "https://gogoanime.cl"
-            },
-            {
-                "id": "clgv4ix5o000269pk6z129dnf",
-                "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=100535",
-                "priority": 2,
-                "url": "https://zoro.to"
-            }
+          {
+            "id": "clgv3xxxx000169pkgb5rdn5l",
+            "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-4"
+          },
+          {
+            "id": "clgv4ix5o000269pk6z129dnf",
+            "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=100535"
+          }
         ]
-    },
-    {
+      },
+      {
         "id": "clh50osw000txpk017u90mhvh",
         "number": 5,
         "title": "New Family",
         "titleVariations": {
-            "native": "新しい家族",
-            "english": "New Family"
+          "native": "新しい家族",
+          "english": "New Family"
         },
         "description": "As Yuuya tries to find a way to level up in the Darkest Regions, he comes across a Black Fenrir pup. Yuuya Tames him, names him Night, and brings him home to live together as family. Side by side, they explore the Darkest Regions...and also go for walks.",
         "image": null,
-        "airedAt": "2023-05-05T00:00:00.000Z",
         "createdAt": "2023-05-01T15:50:33.792Z",
+        "airedAt": "2023-05-05T00:00:00.000Z",
         "sources": [
-            {
-                "id": "clh54n53b0000lopkhmrr2z3x",
-                "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-5",
-                "priority": 1,
-                "url": "https://gogoanime.cl"
-            },
-            {
-                "id": "clh55duja0002lopk2rj476st",
-                "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=100602",
-                "priority": 2,
-                "url": "https://zoro.to"
-            }
+          {
+            "id": "clh54n53b0000lopkhmrr2z3x",
+            "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-5"
+          },
+          {
+            "id": "clh55duja0002lopk2rj476st",
+            "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=100602"
+          }
         ]
-    },
-    {
+      },
+      {
         "id": "clheyzn1k7f5lpk01murasrlv",
         "number": 6,
         "title": "The Sage and Magic",
         "titleVariations": {
-            "native": "賢者と魔法",
-            "english": "The Sage and Magic"
+          "native": "賢者と魔法",
+          "english": "The Sage and Magic"
         },
         "description": "Yuuya has found The Book of the Sage. As he reads it, he realizes that it’s even more amazing than it seems. Meanwhile, the class’s study trip draws near. Yuuya is at the department store with his friends to buy supplies when trouble suddenly strikes.",
         "image": null,
-        "airedAt": "2023-05-12T00:00:00.000Z",
         "createdAt": "2023-05-08T15:00:41.959Z",
+        "airedAt": "2023-05-12T00:00:00.000Z",
         "sources": [
-            {
-                "id": "clhf40ohy0006h9pkhn1ubqmv",
-                "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-6",
-                "priority": 1,
-                "url": "https://gogoanime.cl"
-            },
-            {
-                "id": "clhf4u7230008h9pk1rmccm7v",
-                "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=100817",
-                "priority": 2,
-                "url": "https://zoro.to"
-            }
+          {
+            "id": "clhf40ohy0006h9pkhn1ubqmv",
+            "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-6"
+          },
+          {
+            "id": "clhf4u7230008h9pk1rmccm7v",
+            "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=100817"
+          }
         ]
-    },
-    {
+      },
+      {
         "id": "clhoz2l8t4dzlpk01e43gk5ix",
         "number": 7,
         "title": "An Encounter in the Woods",
         "titleVariations": {
-            "native": "森の中の出会い",
-            "english": "An Encounter in the Woods"
+          "native": "森の中の出会い",
+          "english": "An Encounter in the Woods"
         },
         "description": "Yuuya runs across a girl fighting monsters in the Darkest Regions. She calls herself Luna. They begin spending their days training together, sweating side by side. Back at Ousei Academy, the off-campus study trip begins at a site deep in the mountains.",
         "image": null,
-        "airedAt": "2023-05-19T00:00:00.000Z",
         "createdAt": "2023-05-15T15:00:41.357Z",
+        "airedAt": "2023-05-19T00:00:00.000Z",
         "sources": [
-            {
-                "id": "clhp45ng700076cpk2p51cu1d",
-                "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-7",
-                "priority": 1,
-                "url": "https://gogoanime.cl"
-            },
-            {
-                "id": "clhp4v7a700086cpk3f8k0gma",
-                "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=101364",
-                "priority": 2,
-                "url": "https://zoro.to"
-            }
+          {
+            "id": "clhp45ng700076cpk2p51cu1d",
+            "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-7"
+          },
+          {
+            "id": "clhp4v7a700086cpk3f8k0gma",
+            "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=101364"
+          }
         ]
-    },
-    {
+      },
+      {
         "id": "clhyz5i9v5aslpk01uedj5wzl",
         "number": 8,
         "title": "Off-Campus Study Trip",
         "titleVariations": {
-            "native": "校外学習",
-            "english": "Off-Campus Study Trip"
+          "native": "校外学習",
+          "english": "Off-Campus Study Trip"
         },
         "description": "The study trip isn’t at all what Yuuya and the others had imagined. They’re faced with a series of unexpected challenges. Later, Yuuya meets with Lexia and her guards so they can escort him to his audience with the king, but Night begins acting strangely.",
         "image": null,
-        "airedAt": "2023-05-26T00:00:00.000Z",
         "createdAt": "2023-05-22T15:00:39.234Z",
+        "airedAt": "2023-05-26T00:00:00.000Z",
         "sources": [
-            {
-                "id": "clhz47kqr00048spk5rplbjjt",
-                "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-8",
-                "priority": 1,
-                "url": "https://gogoanime.cl"
-            },
-            {
-                "id": "clhz4x9hj00068spk8nt19mge",
-                "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=101582",
-                "priority": 2,
-                "url": "https://zoro.to"
-            }
+          {
+            "id": "clhz47kqr00048spk5rplbjjt",
+            "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-8"
+          },
+          {
+            "id": "clhz4x9hj00068spk8nt19mge",
+            "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=101582"
+          }
         ]
-    },
-    {
+      },
+      {
         "id": "cli8z8jh8d7repk013veb1q0h",
         "number": 9,
         "title": "The Princess and the Assassin",
         "titleVariations": {
-            "native": "王女と暗殺者",
-            "english": "The Princess and the Assassin"
+          "native": "王女と暗殺者",
+          "english": "The Princess and the Assassin"
         },
         "description": "Lexia has been attacked, a crime punishable by execution. But Yuuya knows the would-be assassin and he can’t help wondering what could have caused them to do such a thing. The answer is shocking...as is the arrival of a new family member for Yuuya.",
         "image": null,
-        "airedAt": "2023-06-02T00:00:00.000Z",
         "createdAt": "2023-05-29T15:00:42.524Z",
+        "airedAt": "2023-06-02T00:00:00.000Z",
         "sources": [
-            {
-                "id": "cli94bxhm00003ypk6bx84x1u",
-                "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-9",
-                "priority": 1,
-                "url": "https://gogoanime.cl"
-            },
-            {
-                "id": "cli951p6s00045vpk0u6o5z6e",
-                "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=101634",
-                "priority": 2,
-                "url": "https://zoro.to"
-            }
+          {
+            "id": "cli94bxhm00003ypk6bx84x1u",
+            "target": "/isekai-de-cheat-skill-wo-te-ni-shita-ore-wa-genjitsu-sekai-wo-mo-musou-suru-level-up-wa-jinsei-wo-kaeta-episode-9"
+          },
+          {
+            "id": "cli951p6s00045vpk0u6o5z6e",
+            "target": "/watch/i-got-a-cheat-skill-in-another-world-and-became-unrivaled-in-the-real-world-too-18343?ep=101634"
+          }
         ]
-    }
-]
-
+      }
+    ],
+    "relations": []
+  }
 export default animeToy
