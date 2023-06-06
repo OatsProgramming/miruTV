0a30f4b1a79e85b7574d7270a0f38a8a3547e0a1
Jack Oats <jackoatmeals@gmail.com>
2023-06-06 04:51:27 -0500

    add: initial implementation of info pg

--- app/info/[animeId]/page.tsx
+++ app/info/[animeId]/page.tsx
@@ -1,7 +1,28 @@
-export default function Page({ params: { animeId} }: {
+import { notFound } from "next/navigation"
+import enimeFetcherToy from "@/lib/toyData/enimeFetcherToy"
+
+export default async function Page({ params: { animeId} }: {
     params: {
         animeId: string
     }
 }) {
 
+    const anime = await enimeFetcherToy({ route: 'anime', arg: animeId })
+    if (!anime) notFound()
+
+    const episodes = anime.episodes
+    return (
+        <div>
+            <div>
+                {anime.title.english}
+            </div>
+            {episodes.map(ep => (
+                <div key={ep.id}>
+                    <div>
+                        {ep.title}
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
 }
