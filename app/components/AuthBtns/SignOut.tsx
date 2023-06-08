2ff16a15296007cc861ffd264d5ec4963275d907
Jack Oats <jackoatmeals@gmail.com>
2023-06-08 16:50:21 -0500

    update: seems that auth is fixed after updating to ^13.4

--- app/components/AuthBtns/SignOut.tsx
+++ app/components/AuthBtns/SignOut.tsx
@@ -1,23 +1,29 @@
 'use client'
 
-import useUrl from "@/lib/zustand/useUrl"
+import toggleDialog from "@/lib/toggleDialog"
 import { signOut } from "next-auth/react"
 import { useRouter } from "next/navigation"
+import type { RefObject, PointerEvent } from "react"
 
-export default function SignOut() {
-    const { url, setUrl } = useUrl()
+export default function SignOut({ dialogRef }: {
+    dialogRef?: RefObject<HTMLDialogElement>
+}) {
     const router = useRouter()
 
-    async function handleSignOut() {
-        // When signing out, prevent user being redirect to the callbackUrl
-        await signOut({ callbackUrl: url, redirect: false })
-            .then(_ => setUrl(''))
+    async function handleSignOut(e: PointerEvent) {
+        // Seems that the bugs present w/ auth is fixed with ^13.4
+        await signOut({ redirect: false })
+            .then(_ => dialogRef && toggleDialog(e, dialogRef))
         router.refresh()
     }
 
     return (
-        <button onClick={handleSignOut}>
-            Sign out
+        <button onPointerDown={handleSignOut}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+                <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+            </svg>
+            <div>Sign Out</div>
         </button>
     )
 }
