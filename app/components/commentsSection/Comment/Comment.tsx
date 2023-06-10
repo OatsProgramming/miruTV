8e6d923e4cee525ad120732cd8302bdf32ba18d8
Jack Oats <jackoatmeals@gmail.com>
2023-06-10 00:58:27 -0500

    add: comment menu can now close if user clicks non menu related nodes & code refactor

--- app/components/commentsSection/Comment/Comment.tsx
+++ app/components/commentsSection/Comment/Comment.tsx
@@ -2,28 +2,27 @@
 
 import { useSession } from "next-auth/react"
 import styles from './comment.module.css'
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import mutatingFetcher from "@/lib/fetchers/mutatingFetcher"
 import type { Comment } from '@prisma/client'
 import type { PointerEvent } from "react"
 import { commentMaxChar } from "../addComment/AddComment"
 import notify from "@/lib/toast/toast"
-import { useSWRConfig } from "swr"
 import useComments from "../hooks/useComments"
-import toggleDialog from "@/lib/toggleDialog"
 
 export default function Comment({ comment }: {
     comment: Comment
 }) {
-    const { data, status } = useSession()
+    const { data } = useSession()
     const [isEditing, setIsEditing] = useState(false)
+    const menuBtnRef = useRef<HTMLDivElement>(null)
     const dialogRef = useRef<HTMLDialogElement>(null)
     const textareaRef = useRef<HTMLTextAreaElement>(null)
     const { refreshComments } = useComments(comment.epId)
 
-    function editComment(e: PointerEvent) {
+    function mutateComment(e: PointerEvent) {
         const el = e.target as HTMLButtonElement
-        const action = el.id as Exclude<Method, 'GET' | 'POST'>
+        const action = el.dataset.httpMethod as Exclude<Method, 'GET' | 'POST'>
         let req: CommentRequest = {
             commentId: comment.id
         }
@@ -42,15 +41,73 @@ export default function Comment({ comment }: {
             // Dont forget to revalidate the data
             .then(res => 'message' in res
                 ? notify({ type: 'error', message: res.message })
-                : refreshComments() 
+                : refreshComments()
             )
             .catch(err => console.error(err))
-        
+
         // Clean up
         dialogRef.current?.close()
         isEditing && setIsEditing(false)
     }
 
+    function handleCancel() {
+        const textarea = textareaRef.current
+        if (!textarea) return
+        textarea.value = ''
+        setIsEditing(false)
+    }
+
+    function handleMutate(e: PointerEvent) {
+        dialogRef.current?.close()
+        mutateComment(e)
+    }
+
+    function handleEdit() {
+        setIsEditing(true)
+        dialogRef.current?.close()
+    }
+
+    // For comment menu
+    useEffect(() => {
+        function handleClose(e: Event) {
+            const dialog = dialogRef.current
+            const menuBtn = menuBtnRef.current
+            if (!dialog || !menuBtn) return
+            // Only care abt the dialog being open
+            else if (!dialog.open) return
+            const el = e.target as HTMLElement
+
+            // True if user clicks outside of current dialog && current dialog is open
+            const scenarioOne = (el === dialog)
+
+            // True if user clicks dialogChild
+            const dialogWeakSet = new WeakSet(dialog.children)
+            const scenarioTwo = dialogWeakSet.has(el)
+
+            // True if user clicks menuBtn (for initial)
+            const scenarioThree = (el === menuBtn)
+
+            // True if user clicks MenuBtnChild
+            const menuWeakSet = new WeakSet(menuBtn.children)
+            const scenarioFour = menuWeakSet.has(el)
+
+            // User clicks non dialog related nodes while current dialog is open
+            if (!scenarioOne && !scenarioTwo) {
+                // User clicks non menu related nodes
+                if (!scenarioThree && !scenarioFour) {
+                    dialog.close()
+                }
+            }
+        }
+
+        window.addEventListener('pointerdown', handleClose)
+        return () => {
+            window.removeEventListener('pointerdown', handleClose)
+        }
+    }, [])
+
+
+    // Note to self: Do this at the end
     let optionBtns = (
         <>
             <button>Report</button>
@@ -62,10 +119,10 @@ export default function Comment({ comment }: {
         if (userId === comment.userId) {
             optionBtns = (
                 <>
-                    <button onPointerDown={() => setIsEditing(true)}>
+                    <button onPointerDown={handleEdit}>
                         Edit
                     </button>
-                    <button id='DELETE' onPointerDown={(e) => editComment(e)}>
+                    <button data-http-method='DELETE' onPointerDown={handleMutate}>
                         Delete
                     </button>
                 </>
@@ -73,49 +130,42 @@ export default function Comment({ comment }: {
         }
     }
 
-    function handleCancel() {
-        const textarea = textareaRef.current
-        if (!textarea) return
-        textarea.value = ''
-        setIsEditing(false)
-    }
-
     return (
         <div className={styles['container']}>
-            {isEditing ? (
-                <>
-                    <textarea
-                        ref={textareaRef}
-                        className={styles['editComment']}
-                        placeholder={comment.body}
-                        maxLength={commentMaxChar}
-                        spellCheck
-                    />
-                    <button id="PATCH" onPointerDown={(e) => editComment(e)}>
-                        Save
-                    </button>
-                    <button onPointerDown={handleCancel}>
-                        Cancel
-                    </button>
-                </>
-            ) : (
-                <div className={styles['text']}>
-                    <div>{comment.userId}</div>
-                    <div>{comment.createdBy}</div>
-                    <div>{comment.body}</div>
-                </div>
-            )}
-            <div className={styles['icon']} onPointerDown={() => dialogRef.current?.show()}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
-                </svg>
+            <div className={styles['comment']}>
+                {isEditing ? (
+                    <>
+                        <textarea
+                            ref={textareaRef}
+                            className={styles['editComment']}
+                            placeholder={comment.body}
+                            maxLength={commentMaxChar}
+                            spellCheck
+                        />
+                        <button data-http-method="PATCH" onPointerDown={handleMutate}>
+                            Save
+                        </button>
+                        <button onPointerDown={handleCancel}>
+                            Cancel
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <div className={styles['text']}>
+                            <div>{comment.createdBy}</div>
+                            <p>{comment.body}</p>
+                        </div>
+                        <div ref={menuBtnRef} className={styles['btn']} onPointerDown={() => dialogRef.current?.show()}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                            </svg>
+                        </div>
+                    </>
+                )}
             </div>
-            <dialog ref={dialogRef}>
+            <dialog ref={dialogRef} className={styles['menu']}>
                 {optionBtns}
-                <button onPointerDown={() => dialogRef.current?.close()}>
-                    Close
-                </button>
             </dialog>
-        </div>
+        </div >
     )
 }
