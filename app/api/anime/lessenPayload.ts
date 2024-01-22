3e6d5131cb5745a685a2bd2ebd0dd026706c71e7
Jack Oats <jackoatmeals@gmail.com>
2024-01-22 16:21:35 -0600

    add: data for anime info (nextAiringEpisode); fix: anime info (artwork) issue

--- app/api/anime/lessenPayload.ts
+++ app/api/anime/lessenPayload.ts
@@ -70,8 +70,12 @@ export default function lessenPayload(
             })
         }
         
-        // too much unnecessary data... just get the first few objs for poster / banner img
-        animeObj.artwork.splice(5, animeObj.artwork.length)
+        // too much unnecessary data... dont want it too unnecessarily big when caching
+        // artwork
+        animeObj.artwork = animeObj.artwork.filter(art => art.type === 'banner')
+        animeObj.artwork.splice(3, animeObj.artwork.length)
+        // recommendations
+        animeObj.recommendations?.splice(5, animeObj.recommendations.length)
     }
 }
 
