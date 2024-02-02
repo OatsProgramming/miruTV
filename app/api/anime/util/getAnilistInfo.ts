9af0dc62c75293fefd9ec7c60cc5383ec89e8bb6
Jack Oats <jackoatmeals@gmail.com>
2024-02-02 02:01:00 -0600

    update: v name; attempt to make anilist info fetching faster

--- app/api/anime/util/getAnilistInfo.ts
+++ app/api/anime/util/getAnilistInfo.ts
@@ -8,20 +8,18 @@ export default async function getAnilistInfo(url: string) {
 
         if (!res.ok) return Promise.reject('Failed to fetch anilist info')
 
-        else return res.json()
+        const result = await res.json() as IAnimeInfo
+        if (result.episodes?.length === 0) return Promise.reject('No episodes')
+
+        return result
     })
 
     // Changed this from Promise.any to Promise.allSettled
     // Must guarantee that it does or doesnt have episodes
     const results = await Promise.allSettled(promiseArr)
-    const re = results.find(src => {
-        if (src.status === 'rejected') return 0
-        else if ((src.value as IAnimeInfo).episodes?.length === 0) return 0
 
-        return true
-    }) as { status: 'fulfilled', value: IAnimeInfo } | undefined
-
-    return re 
-        ? re.value
+    const result = results[0]
+    return result.status === 'fulfilled' 
+        ? result.value
         : null
 }
